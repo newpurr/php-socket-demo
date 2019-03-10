cb27ace77b6f641430ad59901bc45810e4b20790
阿涛 <2417599488@qq.com>
2019-03-10 15:50:20 +0800

    调整socket IO复用实现代码

--- multiplexing/select_server.php
+++ multiplexing/select_server.php
@@ -3,19 +3,19 @@
 // IO多路复用,一个进程/线程可以处理多个连接
 
 // 创建socket
-$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-if (!$socket) {
+$serverSocket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+if (!$serverSocket) {
     die('create server fail');
 }
 
 // 给套接字绑定ip+port
-$ret = socket_bind($socket, '0.0.0.0', 80925);
+$ret = socket_bind($serverSocket, '0.0.0.0', 80925);
 if (!$ret) {
     die('bind server fail');
 }
 
 // 监听socket上的连接
-$ret = socket_listen($socket, 2);
+$ret = socket_listen($serverSocket, 2);
 if (!$ret) {
     die('listen server fail');
 }
@@ -26,14 +26,14 @@ $writefds    = [];
 $exceptfds   = [];
 $connections = [];
 while (true) {
-    $readfds = array_merge($connections, [$socket]);
+    $readfds = array_merge($connections, [$serverSocket]);
     
     if (socket_select($readfds, $writefds, $exceptfds, 2)) {
         // 如果是当前服务端的监听连接
-        if (in_array($socket, $readfds, true)) {
+        if (in_array($serverSocket, $readfds, true)) {
             echo "socket_accept\n";
             // 接受客户端连接
-            $newconn = socket_accept($socket);
+            $newconn = socket_accept($serverSocket);
             $i       = (int)$newconn;
             
             // 将当前客户端连接放入 socket_select 选择
@@ -43,15 +43,15 @@ while (true) {
             $writefds[$i] = $newconn;
             
             echo "Client $i come.\n";
-            
-            $key = array_search($socket, $readfds, true);
-            unset($readfds[$key]);
         }
         // 轮循读通道
         foreach ($readfds as $rfd) {
-            // 客户端连接
-            $i = (int)$rfd;
+            // 排除服务端的可读文件描述符
+            if ($rfd === $serverSocket) {
+                continue;
+            }
             // 从通道读取
+            $i = (int)$rfd;
             $line = @socket_read($rfd, 2048, PHP_NORMAL_READ);
             if ($line === false) {
                 // 读取不到内容，结束连接
@@ -66,7 +66,7 @@ while (true) {
             }
             // 处理逻辑
             $line = trim($line);
-            if ($line === "quit") {
+            if ($line === 'quit') {
                 echo "Client $i quit.\n";
                 closeConn($i);
                 break;
